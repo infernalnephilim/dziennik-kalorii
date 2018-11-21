1087ab513ce2a6b26f523a9ca7b21abbedd50b4e
Aleksandra Półtorak <xmacabrax@gmail.com>
2018-11-21 03:09:51 +0100

    Dodanie nagłówka

---
+++ src/components/header/Header.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import '../../App.css';
+
+class Header extends Component {
+    render() {
+        return (
+            <header className="App__header">
+                <h1>Dziennik kalorii</h1>
+            </header>
+        );
+    }
+}
+
+export default Header;
